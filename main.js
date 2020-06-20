d9b8433bdf34ca9afc718e219722dcb1e9432b11
halakhellow <halakhellow1996@gmail.com>
2020-06-20 17:19:57 +0300

    Test

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 "use strict";
-let movieTitle = document.getElementById("movie-title"),
-    searchButton = document.getElementById("btn"),
+let movieSearch = document.getElementById("movie-search"),
+    searchButton = document.getElementById("search-btn"),
+    latestMovies = document.getElementById("latest-movies-btn"),
     movieDetails = document.getElementById("movie-details");
 
-movieTitle.addEventListener("keydown", function (event) {
+movieSearch.addEventListener("keydown", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         getDetails();
@@ -15,23 +16,49 @@ searchButton.onclick = function () {
 };
 
 function getDetails() {
-    if (movieTitle.value === "") {
+    if (movieSearch.value === "") {
         movieDetails.innerHTML = "";
         movieDetails.innerHTML = "Please Enter a Movie Title To Search";
     } else {
         movieDetails.innerHTML = "";
-        fetch(`https://www.omdbapi.com/?s=${movieTitle.value}&apikey=7e9170bb`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=9fce1e77cbf1f8f4eb80c8366d686cfc&query=${movieSearch.value}`)
             .then((res) => res.json())
             .then((data) => {
-                if (data.Response == "False") {
+                console.log(data);
+                if (data.results.length == 0) {
                     movieDetails.innerHTML = "This is not a Movie name";
                 } else {
-                    let title = data.Search[0].Title,
-                        year = data.Search[0].Year,
-                        imdbUrl = `https://www.imdb.com/title/${data.Search[0].imdbID}/`,
-                        posterUrl = data.Search[0].Poster;
-                    movieDetails.innerHTML = "<h1>" + title + "</h1> <br> <img src='" + posterUrl + "'> <br> <p> Year Released : " +
-                        year + "</p> <br> <p> IMDB Page : <a href='" + imdbUrl + "' target='_blank'> Click Here</a> For More Details </p>";
+                    data.results.map((i) => {
+                        let movieCard = document.createElement("div"),
+                            movieTitleTag = document.createElement("h4"),
+                            movieTitle = document.createTextNode(i.title),
+                            posterContainer = document.createElement("div"),
+                            moviePoster = document.createElement("img"),
+                            contentContainer = document.createElement("div"),
+                            yearTag = document.createElement("p"),
+                            year = document.createTextNode(`Year Released : ${i.release_date}`),
+                            tmdbUrl = document.createElement("a");
+
+                        movieCard.classList.add("card");
+                        moviePoster.setAttribute("src", `https://image.tmdb.org/t/p/w500/${i.poster_path}`);
+                        tmdbUrl.href = `https://www.themoviedb.org/movie/${i.id}`;
+                        tmdbUrl.setAttribute("target", "_blank");
+                        movieTitleTag.appendChild(movieTitle);
+                        yearTag.appendChild(year);
+                        tmdbUrl.appendChild(moviePoster);
+                        posterContainer.appendChild(tmdbUrl);
+                        posterContainer.classList.add("poster-container");
+                        contentContainer.appendChild(movieTitleTag);
+                        contentContainer.appendChild(yearTag);
+                        contentContainer.classList.add("content-container");
+
+                        movieCard.appendChild(posterContainer);
+                        movieCard.appendChild(contentContainer);
+                        movieDetails.appendChild(movieCard);
+                    })
+
+
+
                 }
             })
     }
