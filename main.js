58719b9a7324a6316e599182b3e506335f7f0ad2
Hala Alkhellow <halakhellow1996@gmail.com>
2020-06-22 20:26:44 +0300

    get popular movies

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 let movieSearch = document.getElementById("movie-search"),
     searchButton = document.getElementById("search-btn"),
-    latestMovies = document.getElementById("latest-movies-btn"),
+    popularMovies = document.getElementById("popular-movies-btn"),
     alertMsg = document.getElementById("alert-msg"),
     movieDetails = document.getElementById("movie-details");
 
@@ -12,8 +12,46 @@ movieSearch.addEventListener("keydown", function (event) {
     }
 });
 
-searchButton.onclick = function () {
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
     getDetails();
+});
+popularMovies.addEventListener("click", function (event) {
+    event.preventDefault();
+    getPopular();
+});
+
+function makeCards(i) {
+    let movieCard = document.createElement("div"),
+        movieTitleTag = document.createElement("h4"),
+        movieTitle = document.createTextNode(i.title),
+        moviePoster = document.createElement("img"),
+        contentContainer = document.createElement("div"),
+        yearTag = document.createElement("p"),
+        year = document.createTextNode(`Year Released : ${i.release_date}`),
+        tmdbUrl = document.createElement("a");
+
+    movieCard.classList.add("card", "border-dark");
+    moviePoster.setAttribute("src", `https://image.tmdb.org/t/p/w500/${i.poster_path}`);
+    if (i.poster_path == null) {
+        moviePoster.setAttribute("src", "assets/images/noimage.png");
+    }
+    moviePoster.classList.add("card-img-top");
+    tmdbUrl.href = `https://www.themoviedb.org/movie/${i.id}`;
+    tmdbUrl.setAttribute("target", "_blank");
+    movieTitleTag.appendChild(movieTitle);
+    movieTitleTag.classList.add("card-title");
+    yearTag.appendChild(year);
+    yearTag.classList.add("card-text");
+    tmdbUrl.appendChild(moviePoster);
+    contentContainer.appendChild(movieTitleTag);
+    contentContainer.appendChild(yearTag);
+    contentContainer.classList.add("card-body");
+
+    movieCard.appendChild(tmdbUrl);
+    movieCard.appendChild(contentContainer);
+    movieDetails.appendChild(movieCard);
+
 };
 
 function getDetails() {
@@ -26,48 +64,27 @@ function getDetails() {
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=9fce1e77cbf1f8f4eb80c8366d686cfc&query=${movieSearch.value}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
                 if (data.results.length == 0) {
                     alertMsg.innerHTML = "This is not a movie name!";
                     alertMsg.style.display = "block";
                 } else {
                     alertMsg.style.display = "none";
-                    data.results.map((i) => {
-                        let movieCard = document.createElement("div"),
-                            movieTitleTag = document.createElement("h4"),
-                            movieTitle = document.createTextNode(i.title),
-                            moviePoster = document.createElement("img"),
-                            contentContainer = document.createElement("div"),
-                            yearTag = document.createElement("p"),
-                            year = document.createTextNode(`Year Released : ${i.release_date}`),
-                            tmdbUrl = document.createElement("a");
-
-                        movieCard.classList.add("card", "border-dark");
-                        moviePoster.setAttribute("src", `https://image.tmdb.org/t/p/w500/${i.poster_path}`);
-                        if (i.poster_path == null) {
-                            moviePoster.setAttribute("src", "assets/images/noimage.png");
-                        }
-                        moviePoster.classList.add("card-img-top");
-                        tmdbUrl.href = `https://www.themoviedb.org/movie/${i.id}`;
-                        tmdbUrl.setAttribute("target", "_blank");
-                        movieTitleTag.appendChild(movieTitle);
-                        movieTitleTag.classList.add("card-title");
-                        yearTag.appendChild(year);
-                        yearTag.classList.add("card-text");
-                        tmdbUrl.appendChild(moviePoster);
-                        contentContainer.appendChild(movieTitleTag);
-                        contentContainer.appendChild(yearTag);
-                        contentContainer.classList.add("card-body");
-
-                        movieCard.appendChild(tmdbUrl);
-                        movieCard.appendChild(contentContainer);
-                        movieDetails.appendChild(movieCard);
-
+                    data.results.map((result) => {
+                        makeCards(result);
                     })
-
-
-
                 }
             })
     }
-}
+};
+
+function getPopular() {
+    movieDetails.innerHTML = "";
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=9fce1e77cbf1f8f4eb80c8366d686cfc")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            data.results.map((popular) => {
+                makeCards(popular);
+            })
+        })
+};
